6725d5fc1547bf52a3d86716e01f229007bfe7b6
Gideon Scheepers <dot.kiddo@gmail.com>
2023-09-23 11:55:13 +0200

    - merge underlying tables

--- webapi/DbScripts/002_SeedData.sql
+++ webapi/DbScripts/002_SeedData.sql
@@ -1,28 +1,12 @@
-SET IDENTITY_INSERT Categories ON
-
-INSERT INTO Categories (Id, Name) VALUES 
-(1, 'Action'),
-(2, 'Animation'),
-(3, 'Comedy'),
-(4, 'Drama'),
-(5, 'Fantasy'),
-(6, 'Mystery'),
-(7, 'Science Fiction');
-
-SET IDENTITY_INSERT Categories OFF
-
--------------------------------------------------
-
 SET IDENTITY_INSERT Movies ON
 
-
-INSERT INTO Movies (Id, Name, CategoryId, Rating) VALUES
-(1, 'James Bond Quantum of Solace', 1, 4),
-(2, 'Inside Out', 2, 4),
-(3, 'Jumanji', 3, 4),
-(4, 'Atonement', 4, 5),
-(5, 'Harry Potter and the Philosopher''s Stone', 5, 5),
-(6, 'Murder on the Nile', 6, 3),
-(7, 'Edge of Tomorrow', 7, 4);
+INSERT INTO Movies (Id, Name, Category, Rating) VALUES
+(1, 'James Bond Quantum of Solace', 'Action', 4),
+(2, 'Inside Out', 'Animation', 4),
+(3, 'Jumanji', 'Comedy', 4),
+(4, 'Atonement', 'Drama', 5),
+(5, 'Harry Potter and the Philosopher''s Stone', 'Fantasy', 5),
+(6, 'Murder on the Nile', 'Mystery', 3),
+(7, 'Edge of Tomorrow', 'Science Fiction', 4);
 
 SET IDENTITY_INSERT Movies OFF
